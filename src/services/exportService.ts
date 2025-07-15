cd3cbadfd792322f1f85047e4603c793bc4d2943
Ferabensrl <fnosieski@gmail.com>
2025-07-15 09:57:06 -0300

    Initialize Vite React project

---
+++ src/services/exportService.ts
@@ -0,0 +1,9 @@
+// Placeholder export service
+const exportService = {
+  async exportToExcel(_data: unknown): Promise<void> {
+    // TODO: implement real export logic
+    console.log('Exporting to Excel...', _data);
+  },
+};
+
+export default exportService;
